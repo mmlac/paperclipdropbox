f3915750b894d3f8a80fd9f32b18c6f7908c53ea
Markus Lachinger <markus.lachinger@sv.cmu.edu>
2011-11-11 10:52:31 -0800

    fixed railtie_name

--- lib/paperclipdropbox/railtie.rb
+++ lib/paperclipdropbox/railtie.rb
@@ -2,7 +2,7 @@ require 'paperclipdropbox'
 require 'rails'
 module Paperclipdropbox
   class Railtie < Rails::Railtie
-    railtie_name :paperclipdropbox
+    #railtie_name :paperclipdropbox
 
     rake_tasks do
       load "tasks/paperclipdropbox.rake"
